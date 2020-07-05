f98b0a9dd653f6814667bf379ca5e654d598011d
andre <andre.liu@schibsted.com>
2020-07-05 21:51:33 +0200

    Vue, webpack & workflow

---
+++ src/options/Options.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    This is options page in /options
+  </div>
+</template>
+<script>
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Options extends Vue {
+};
+</script>
---
+++ src/popup/Popup.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    This is Popup in /popup
+  </div>
+</template>
+<script>
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Popup extends Vue {
+};
+</script>
