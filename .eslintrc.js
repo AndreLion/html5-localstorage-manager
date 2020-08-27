5e0ec7300803e5cf11d0aba413d2d2c7d5add9a6
andre <andre.liu@schibsted.com>
2020-08-27 11:45:59 +0200

    Initial Commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  parserOptions: {
+    parser: "babel-eslint",
+    ecmaFeatures: {
+      legacyDecorators: true
+    }
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+  },
+  globals: {
+    chrome: "readonly"
+  }
+};
